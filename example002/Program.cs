b25f76fad559529c62d29a2822d337d039042f37
«Vova <vrobskiy@gmail.com>
2022-04-11 20:41:29 +0300

    add example 002

---
+++ example002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberA * numberA == numberB)
+{
+    Console.WriteLine("Да!");
+}
+else 
+{
+    Console.WriteLine("Нет!");
+}
+
