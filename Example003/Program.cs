e4d42385aac33dc1f3bccba71a710f4ba4819de7
«Vova <vrobskiy@gmail.com>
2022-04-11 20:48:03 +0300

    add Example 003

---
+++ Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int  number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
